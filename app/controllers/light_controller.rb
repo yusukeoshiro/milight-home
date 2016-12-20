3fa163a5bb1ab4b145ebd9794d416fa62eec16e9
Yusuke Oshiro <yoshiro@yoshiro-ltm.internal.salesforce.com>
2016-12-20 12:03:27 +0900

    alexa response added

--- app/controllers/light_controller.rb
+++ app/controllers/light_controller.rb
@@ -163,29 +163,27 @@ class LightController < ApplicationController
 
 		when "TurnOffEverything"
 			send_ir_signal( "IR_AC_OFF" )
-			$bridge.all_off
-			response = {
-				"version" => "1.0",
-				"response" => {
-					"outputSpeech" => {
-						"type" => "PlainText",
-						"text" => "ok, I've turned off everything!"
-					},
-					"shouldEndSession" => true
-				}
-			}
-			render :json => response.to_json				
-
-
+			$bridge.all_off			
+			render :json => alexa_response( "ok, I've turned off everything!" )				
 		else
 		end
 
-
-
-		pp params
-
 	end
 
+
+	def alexa_response( speech )
+		response = {
+			"version" => "1.0",
+			"response" => {
+				"outputSpeech" => {
+					"type" => "PlainText",
+					"text" => speech,
+				},
+				"shouldEndSession" => true
+			}
+		}
+		return response.to_json
+	end
 
 	def send_ir_signal( sig_name )
 		uri = URI.parse(ENV["IR_KIT_ENDPOINT"])
